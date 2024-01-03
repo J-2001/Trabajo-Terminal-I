702a4d3e7e0140ebe1b4159e3f4932a5f663f5de
J-2001 <roldangomezjuan01@gmail.com>
2024-01-02 18:53:43 -0600

    Implementación del Analizador

--- app/src/main/java/com/example/prototipo2/Analizador.java
+++ app/src/main/java/com/example/prototipo2/Analizador.java
@@ -13,12 +13,15 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Analizador extends Service {
 
     private static final String CHANNEL_ID = "AnalizadorConsumoDeEnergia";
+    private static final DecimalFormat df = new DecimalFormat("0.00");
     private int previousRowId;
     private int previousRowChargeCounter;
     private long previousRowTimeStamp;
@@ -26,10 +29,12 @@ public class Analizador extends Service {
     private int currentRowChargeCounter;
     private long currentRowTimeStamp;
     private float averageVoltage;
+    private int voltageCounter;
     private int status;
     private float ccpm; // x
     private float media;
-    private float desvEst;//
+    private float desvEst;
+    private float pz;
     private Bateria bateria;
     private Escaneo escaneo;
     private Timer timer;
@@ -44,10 +49,12 @@ public class Analizador extends Service {
         currentRowChargeCounter = 0;
         currentRowTimeStamp = 0;
         averageVoltage = 0;
+        voltageCounter = 0;
         status = 0;
         ccpm = 0;
         media = 0;
         desvEst = 0;
+        pz = 0;
         bateria = new Bateria(getApplicationContext());
         escaneo = new Escaneo(getApplicationContext());
         timer = new Timer();
@@ -59,6 +66,7 @@ public class Analizador extends Service {
         currentRowChargeCounter = bateria.getChargeCounter();
         currentRowTimeStamp = bateria.getTimeStamp();
         averageVoltage = bateria.getVoltage();
+        voltageCounter += 1;
 
         escaneo.setStartId(currentRowId);
         escaneo.setStartTimeStamp(currentRowTimeStamp);
@@ -74,7 +82,8 @@ public class Analizador extends Service {
                     Log.d("Bateria", "Nuevo registro!");
                     currentRowChargeCounter = bateria.getChargeCounter();
                     currentRowTimeStamp = bateria.getTimeStamp();
-                    averageVoltage = (float) ((averageVoltage + bateria.getVoltage()) / 2.0);
+                    averageVoltage += bateria.getVoltage();
+                    voltageCounter += 1;
                     status = bateria.getStatus();
 
                     int cc = Math.abs(currentRowChargeCounter - previousRowChargeCounter);
@@ -82,15 +91,24 @@ public class Analizador extends Service {
                         escaneo.updateDatosConsumo(cc);
                     }
 
-                    ccpm = (float) (cc * 60 / ((currentRowTimeStamp-previousRowTimeStamp)/1000.0));
+                    ccpm = (float) (cc * 60 / ((currentRowTimeStamp - previousRowTimeStamp) / 1000.0));
 
-                    media = 0;
-                    desvEst = 0;
+                    media = updateMedia();
+                    desvEst = updateDesvEst();
+
+                    float x = 0;
+
+                    if (desvEst != 0) {
+                        x = (ccpm - media) / desvEst;
+                    }
+
+                    String z = df.format(x);
 
                     DistribucionNormalEstandar dne = new DistribucionNormalEstandar();
-                    float f = dne.getP(0);
+                    pz = dne.getP(Float.parseFloat(z));
+                    Log.d("P(Z>x)=", String.valueOf(pz));
 
-                    if (f != -1) {
+                    if (pz != -1) {
                         showNotification();
                     }
 
@@ -116,6 +134,7 @@ public class Analizador extends Service {
         timerTask.cancel();
         escaneo.setEndId(currentRowId);
         escaneo.setEndTimeStamp(currentRowTimeStamp);
+        averageVoltage = averageVoltage / voltageCounter;
         escaneo.setAverageVoltage(averageVoltage);
         escaneo.insertIntoDB();
         Log.w("Analizador", "Analizador destruido");
@@ -125,6 +144,38 @@ public class Analizador extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    public float updateMedia() {
+        ArrayList<Float> ccpms = getAllCCpm();
+
+        if (ccpms == null) {
+            return 0;
+        }
+
+        float sum = 0;
+
+        for (Float ccpm : ccpms) {
+            sum += ccpm;
+        }
+
+        return sum / ccpms.size();
+    }
+
+    public float updateDesvEst() {
+        ArrayList<Float> ccpms = getAllCCpm();
+
+        if (ccpms == null) {
+            return 0;
+        }
+
+        float sum = 0;
+
+        for (Float ccpm : ccpms) {
+            sum += Math.pow(ccpm - media, 2);
+        }
+
+        return (float) Math.sqrt(sum / ccpms.size());
     }
 
     public void showNotification() {
@@ -141,6 +192,25 @@ public class Analizador extends Service {
         }
     }
 
+    public ArrayList<Float> getAllCCpm() {
+        try (AnalizadorDBHelper dbHelper = new AnalizadorDBHelper(getApplicationContext())) {
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            String[] columns = {AnalizadorContract.AnalizadorEntry.COLUMN_CCPM};
+            Cursor cursor = db.query(AnalizadorContract.AnalizadorEntry.TABLE_NAME, columns, null, null, null, null, null);
+            ArrayList<Float> ccpms = new ArrayList<>();
+            while (cursor.moveToNext()) {
+                ccpms.add(cursor.getFloat(cursor.getColumnIndexOrThrow(AnalizadorContract.AnalizadorEntry.COLUMN_CCPM)));
+            }
+            cursor.close();
+
+            return ccpms;
+        } catch (Exception e) {
+            Log.e("Analizador.getAllCCpm()", e.toString());
+
+            return null;
+        }
+    }
+
     public String getAllData(Context context) {
         AnalizadorDBHelper dbHelper = new AnalizadorDBHelper(context);
         SQLiteDatabase db = dbHelper.getReadableDatabase();
@@ -149,7 +219,7 @@ public class Analizador extends Service {
         stringBuilder.append(AnalizadorContract.AnalizadorEntry._ID + "," + AnalizadorContract.AnalizadorEntry.COLUMN_PREVIOUS_BATERIA_ID + "," +
                 AnalizadorContract.AnalizadorEntry.COLUMN_CURRENT_BATERIA_ID + "," + AnalizadorContract.AnalizadorEntry.COLUMN_BATTERY_STATUS + "," +
                 AnalizadorContract.AnalizadorEntry.COLUMN_CCPM + "," + AnalizadorContract.AnalizadorEntry.COLUMN_MEDIA + "," +
-                AnalizadorContract.AnalizadorEntry.COLUMN_DESV_EST + ";");
+                AnalizadorContract.AnalizadorEntry.COLUMN_DESV_EST + "," + AnalizadorContract.AnalizadorEntry.COLUMN_PZ + ";");
         while (cursor.moveToNext()) {
             stringBuilder.append(cursor.getInt(cursor.getColumnIndexOrThrow(AnalizadorContract.AnalizadorEntry._ID)));
             stringBuilder.append(",");
@@ -164,6 +234,8 @@ public class Analizador extends Service {
             stringBuilder.append(cursor.getFloat(cursor.getColumnIndexOrThrow(AnalizadorContract.AnalizadorEntry.COLUMN_MEDIA)));
             stringBuilder.append(",");
             stringBuilder.append(cursor.getFloat(cursor.getColumnIndexOrThrow(AnalizadorContract.AnalizadorEntry.COLUMN_DESV_EST)));
+            stringBuilder.append(",");
+            stringBuilder.append(cursor.getFloat(cursor.getColumnIndexOrThrow(AnalizadorContract.AnalizadorEntry.COLUMN_PZ)));
             stringBuilder.append(";");
         }
         cursor.close();
@@ -179,6 +251,7 @@ public class Analizador extends Service {
         values.put(AnalizadorContract.AnalizadorEntry.COLUMN_CCPM, getCcpm());
         values.put(AnalizadorContract.AnalizadorEntry.COLUMN_MEDIA, getMedia());
         values.put(AnalizadorContract.AnalizadorEntry.COLUMN_DESV_EST, getDesvEst());
+        values.put(AnalizadorContract.AnalizadorEntry.COLUMN_PZ, getPz());
 
         return values;
     }
@@ -205,5 +278,9 @@ public class Analizador extends Service {
 
     public float getDesvEst() {
         return desvEst;
+    }
+
+    public float getPz() {
+        return pz;
     }
 }
--- app/src/main/java/com/example/prototipo2/AnalizadorDBHelper.java
+++ app/src/main/java/com/example/prototipo2/AnalizadorDBHelper.java
@@ -15,7 +15,8 @@ public class AnalizadorDBHelper extends SQLiteOpenHelper {
             AnalizadorContract.AnalizadorEntry.COLUMN_BATTERY_STATUS + " INTEGER NOT NULL," +
             AnalizadorContract.AnalizadorEntry.COLUMN_CCPM + " REAL NOT NULL," +
             AnalizadorContract.AnalizadorEntry.COLUMN_MEDIA + " REAL NOT NULL," +
-            AnalizadorContract.AnalizadorEntry.COLUMN_DESV_EST + " REAL NOT NULL)";
+            AnalizadorContract.AnalizadorEntry.COLUMN_DESV_EST + " REAL NOT NULL," +
+            AnalizadorContract.AnalizadorEntry.COLUMN_PZ + " REAL NOT NULL)";
     public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + AnalizadorContract.AnalizadorEntry.TABLE_NAME;
 
     public AnalizadorDBHelper(Context context) {
