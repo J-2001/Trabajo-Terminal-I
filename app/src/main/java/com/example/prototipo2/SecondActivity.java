45480f9ac768d8b5c07f0a773d7402760aa55f27
J-2001 <roldangomezjuan01@gmail.com>
2024-04-19 00:32:54 -0600

    Caso de Uso 1 (Second Activity) Programaticamente Terminado!

--- app/src/main/java/com/example/prototipo2/SecondActivity.java
+++ app/src/main/java/com/example/prototipo2/SecondActivity.java
@@ -6,9 +6,6 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.google.android.material.snackbar.Snackbar;
