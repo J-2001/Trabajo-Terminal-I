616e85e8823289e46fd6a2290f6ef27be79c43a8
J-2001 <roldangomezjuan01@gmail.com>
2024-04-23 19:14:15 -0600

    Programación Avanzada del Caso de Uso 6

--- app/src/main/java/com/example/prototipo2/FourthActivity.java
+++ app/src/main/java/com/example/prototipo2/FourthActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
