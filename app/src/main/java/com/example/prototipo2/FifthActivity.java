2e0662f17fa60a09174f3bce9b0cc771430e9b4c
J-2001 <roldangomezjuan01@gmail.com>
2024-04-25 15:42:14 -0600

    Caso de Uso 7 (Fifth Activity) Programaticamente Terminado!

--- app/src/main/java/com/example/prototipo2/FifthActivity.java
+++ app/src/main/java/com/example/prototipo2/FifthActivity.java
@@ -6,11 +6,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,105 +14,83 @@ import java.util.Random;
 
 public class FifthActivity extends AppCompatActivity {
 
-    private static final int limit = 5;
-    private List<Integer> order = new ArrayList<>();
+    private final int limit = 5;
+    private final List<Integer> order = new ArrayList<>();
     private int pointer = 0;
-    private LinearLayout layout;
-    private ArrayList<FrameLayout> frameLayouts;
-    private Button button;
-    private List<Integer> displayed;
-    private int number;
-    private int counter;
-    // private boolean first;
-
-    private final OnClickListener listener = new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            layout.removeView(button);
-            switch (number) {
-                case 1:
-                    loadFragment(new Consejo01Fragment());
-                    break;
-                case 2:
-                    loadFragment(new Consejo02Fragment());
-                    break;
-                case 3:
-                    loadFragment(new Consejo03Fragment());
-                    break;
-                case 4:
-                    loadFragment(new Consejo04Fragment());
-                    break;
-                case 5:
-                    loadFragment(new Consejo05Fragment());
-                    break;
-            }
-            number = newNumber();
-            if (number != -1) {
-                button = new Button(layout.getContext());
-                button.setText("");
-                button.setOnClickListener(listener);
-                layout.addView(button, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-            }
-        }
-    };
+    private boolean first = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fifth);
 
+        Random random = new Random();
         while (order.size() < limit) {
-            Random random = new Random();
             int r = random.nextInt(limit);
             if (!order.contains(r)) {
                 order.add(r);
             }
         }
 
-        layout = this.findViewById(R.id.fifth_linlay);
-        displayed = new ArrayList<>();
-        number = newNumber();
-        button = new Button(this);
-        button.setText("");
-        button.setOnClickListener(listener);
-        layout.addView(button, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        updateFragment();
 
-        frameLayouts = new ArrayList<>();
-        counter = 0;
-        // first = true;
+        Button previous = this.findViewById(R.id.fifth_btn_prv);
+        Button next = this.findViewById(R.id.fifth_btn_nxt);
+
+        previous.setOnClickListener(v -> {
+            pointer -= 1;
+            updateFragment();
+            if (!next.isEnabled()) {
+                next.setEnabled(true);
+            }
+            if (pointer == 0) {
+                previous.setEnabled(false);
+            }
+        });
+
+        next.setOnClickListener(v -> {
+            pointer += 1;
+            updateFragment();
+            if (!previous.isEnabled()) {
+                previous.setEnabled(true);
+            }
+            if (pointer == limit-1) {
+                next.setEnabled(false);
+            }
+        });
+
+    }
+
+    public void updateFragment() {
+        switch (order.get(pointer)) {
+            case 0:
+                loadFragment(new Consejo01Fragment());
+                break;
+            case 1:
+                loadFragment(new Consejo02Fragment());
+                break;
+            case 2:
+                loadFragment(new Consejo03Fragment());
+                break;
+            case 3:
+                loadFragment(new Consejo04Fragment());
+                break;
+            case 4:
+                loadFragment(new Consejo05Fragment());
+                break;
+        }
     }
 
     public void loadFragment(Fragment fragment) {
-        frameLayouts.add(new FrameLayout(this));
-        frameLayouts.get(counter).setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT));
-        int id = View.generateViewId();
-        frameLayouts.get(counter).setId(id);
-        layout.addView(frameLayouts.get(counter), new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
-        ft.add(id, fragment);
-        /* if (first)  {
+        if (first) {
             ft.add(R.id.fifth_frmlay, fragment);
             first = false;
         } else {
             ft.replace(R.id.fifth_frmlay, fragment);
-        } */
-        ft.commit();
-        counter += 1;
-    }
-
-    public int newNumber() {
-        Random random = new Random();
-        int r;
-        while (true) {
-            r = random.nextInt(limit) + 1;
-            if (displayed.size() == limit) {
-                return -1;
-            } else if (!displayed.contains(r)) {
-                displayed.add(r);
-                return r;
-            }
         }
+        ft.commit();
     }
 
 }
