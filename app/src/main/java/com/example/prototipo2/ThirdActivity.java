a9c5dfbe7c8b9fdf7942c3d0c6d7d2a1eba7d836
J-2001 <roldangomezjuan01@gmail.com>
2024-04-27 18:29:57 -0600

    Programación del Caso de Uso 5

--- app/src/main/java/com/example/prototipo2/ThirdActivity.java
+++ app/src/main/java/com/example/prototipo2/ThirdActivity.java
@@ -31,8 +31,12 @@ import java.util.Map;
 public class ThirdActivity extends AppCompatActivity {
 
     private final List<String> videoStreaming = Arrays.asList("Netflix", "Disney+", "Star+", "Prime Video", "Max", "Crunchyroll", "ViX");
-    private static final int textSize = 11;
-    private int pointer = 0;
+    private static final int textSize = 12;
+    private int[] pointer = {0, 0};
+    private ArrayList<Chip> chips01 = new ArrayList<>();
+    private ArrayList<Chip> chips02 = new ArrayList<>();
+    private ArrayList<Chip> chips03 = new ArrayList<>();
+    private ArrayList<Chip> chips04 = new ArrayList<>();
 
     private boolean btn01_status;
     private boolean btn02_status;
@@ -44,12 +48,38 @@ public class ThirdActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
 
-        ChipGroup chipgrp01 = this.findViewById(R.id.third_chipgrp_01);
-        ChipGroup chipgrp02 = this.findViewById(R.id.third_chipgrp_02);
-        ChipGroup chipgrp03 = this.findViewById(R.id.third_chipgrp_03);
-        ChipGroup chipgrp04 = this.findViewById(R.id.third_chipgrp_04);
+        LineChart lineChart = this.findViewById(R.id.third_chart_01);
+        lineChart.getXAxis().setValueFormatter(new TimeStampAxisValueFormatter());
+        lineChart.getXAxis().setLabelRotationAngle(270);
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        lineChart.getAxisRight().setEnabled(false);
 
-        ArrayList<Chip> chips01 = new ArrayList<>();
+        List<Entry> entries = new ArrayList<>();
+        Bateria bateria = new Bateria(getApplicationContext());
+        Map<Long, Integer> data = bateria.getAllRowsData();
+        Map<Float, Integer> dataF = formatter(data);
+        for (Float f : dataF.keySet()) {
+            entries.add(new Entry(f, dataF.get(f)));
+        }
+        Map<Long, Integer> test = new LinkedHashMap<>();
+        test.put(1714109070663L, 1);
+        test.put(1714109170663L, 3);
+        test.put(1714109270663L, 1);
+        test.put(1714109370663L, 4);
+        Map<Float, Integer> testF = formatter(test);
+        for (Float f : testF.keySet()) {
+            entries.add(new Entry(f, testF.get(f)));
+        }
+
+        LineDataSet lineDataSet = new LineDataSet(entries, "MyLabel");
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+
+        Escaneo escaneo = new Escaneo(getApplicationContext());
+
+        ChipGroup chipgrp01 = this.findViewById(R.id.third_chipgrp_01);
+
         for (int i = 0; i < videoStreaming.size(); i++) {
             chips01.add(new Chip(this));
             chips01.get(i).setText(videoStreaming.get(i));
@@ -63,16 +93,74 @@ public class ThirdActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(@NonNull ChipGroup chipGroup, @NonNull List<Integer> list) {
                 Log.i("Pruebas(01): ", list.toString());
+                if (pointer[1] == 0) {
+                    if (list.isEmpty()) {
+                        lineChart.setData(lineData);
+                        lineChart.invalidate();
+                    } else {
+                        LineData nLineData = new LineData();
+                        for (int id : list) {
+                            List<Entry> nEntries = new ArrayList<>();
+                            Map<Long, Integer> nData = escaneo.getScansDataFilteredByVideoStreaming(videoStreaming.get(id-1));
+                            /*if (nData.isEmpty()) {
+                                chips01.get(id-1).setEnabled(false);
+                                return;
+                            }*/
+                            Map<Float, Integer> nDataF = formatter(nData);
+                            for (Float f : nDataF.keySet()) {
+                                nEntries.add(new Entry(f, nDataF.get(f)));
+                            }
+                            LineDataSet nLineDataSet = new LineDataSet(nEntries, videoStreaming.get(id-1));
+                            nLineData.addDataSet(nLineDataSet);
+                            if (id == 1) {
+                                List<Entry> xentries = new ArrayList<>();
+                                Map<Long, Integer> ntest = new LinkedHashMap<>();
+                                ntest.put(1714109070663L, 4);
+                                ntest.put(1714109170663L, 3);
+                                ntest.put(1714109270663L, 2);
+                                ntest.put(1714109370663L, 1);
+                                Map<Float, Integer> ntestF = formatter(ntest);
+                                for (Float f : ntestF.keySet()) {
+                                    xentries.add(new Entry(f, ntestF.get(f)));
+                                }
+
+                                LineDataSet xlineDataSet = new LineDataSet(xentries, "MyLabel22");
+                                nLineData.addDataSet(xlineDataSet);
+                            }
+                            if (id == 2) {
+                                List<Entry> xentries = new ArrayList<>();
+                                Map<Long, Integer> ntest = new LinkedHashMap<>();
+                                ntest.put(1714109070663L, 1);
+                                ntest.put(1714109170663L, 2);
+                                ntest.put(1714109270663L, 3);
+                                ntest.put(1714109370663L, 5);
+                                Map<Float, Integer> ntestF = formatter(ntest);
+                                for (Float f : ntestF.keySet()) {
+                                    xentries.add(new Entry(f, ntestF.get(f)));
+                                }
+
+                                LineDataSet xlineDataSet = new LineDataSet(xentries, "MyLabel33");
+                                nLineData.addDataSet(xlineDataSet);
+                            }
+                        }
+                        lineChart.setData(nLineData);
+                        lineChart.invalidate();
+                    }
+                }
             }
         });
 
-        Log.i("Pruebas(02): ", "isCheckable(): " + chips01.get(0).isCheckable());
-        chips01.get(0).setChecked(true);
-        chips01.get(0).setChecked(false);
+        ChipGroup chipgrp02 = this.findViewById(R.id.third_chipgrp_02);
+        ChipGroup chipgrp03 = this.findViewById(R.id.third_chipgrp_03);
+        ChipGroup chipgrp04 = this.findViewById(R.id.third_chipgrp_04);
 
         Button btn01 = this.findViewById(R.id.third_btn_01);
         btn01.setOnClickListener(v -> {
-            pointer = 0;
+            if (pointer[1] != 0) {
+                pointer[0] = pointer[1];
+                pointer[1] = 0;
+                clearFilters();
+            }
             chipgrp01.setVisibility(View.VISIBLE);
             chipgrp02.setVisibility(View.GONE);
             chipgrp03.setVisibility(View.GONE);
@@ -102,36 +190,6 @@ public class ThirdActivity extends AppCompatActivity {
             chipgrp03.setVisibility(View.GONE);
             chipgrp04.setVisibility(View.VISIBLE);
         });
-
-        LineChart chart01 = this.findViewById(R.id.third_chart_01);
-        chart01.getXAxis().setValueFormatter(new TimeStampAxisValueFormatter());
-        chart01.getXAxis().setLabelRotationAngle(270);
-        chart01.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        chart01.getAxisRight().setEnabled(false);
-
-        List<Entry> entries = new ArrayList<>();
-        Bateria bateria = new Bateria(getApplicationContext());
-        Map<Long, Integer> data = bateria.getAllRowsData();
-        Map<Float, Integer> dataF = formatter(data);
-        for (Float f : dataF.keySet()) {
-            entries.add(new Entry(f, dataF.get(f)));
-        }
-        Map<Long, Integer> test = new LinkedHashMap<>();
-        test.put(1714109070663L, 1);
-        test.put(1714109170663L, 3);
-        test.put(1714109270663L, 1);
-        test.put(1714109370663L, 4);
-        Map<Float, Integer> testF = formatter(test);
-        for (Float f : testF.keySet()) {
-            entries.add(new Entry(f, testF.get(f)));
-        }
-
-        LineDataSet dataSet = new LineDataSet(entries, "MyLabel");
-        LineData lineData = new LineData(dataSet);
-        chart01.setData(lineData);
-        chart01.invalidate();
-
-
 
         /*/ TableLayouts
         TableLayout tabLay01 = this.findViewById(R.id.third_tablay_01);
@@ -298,9 +356,12 @@ public class ThirdActivity extends AppCompatActivity {
         });*/
     }
 
-    private void clearFilter() {
-        switch (pointer) {
+    private void clearFilters() {
+        switch (pointer[0]) {
             case 0:
+                for (Chip c : chips01) {
+                    c.setChecked(false);
+                }
                 break;
             case 1:
                 break;
