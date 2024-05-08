60bb505294b4306b48af5834ad57e696015d5ba0
J-2001 <roldangomezjuan01@gmail.com>
2024-05-08 00:04:51 -0600

    Envío de Datos al Servidor Programaticamente Terminado!

---
+++ app/src/main/java/com/example/prototipo2/UploadWorker.java
@@ -0,0 +1,81 @@
+package com.example.prototipo2;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class UploadWorker extends Worker {
+
+    public UploadWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        String[] columns = {AnalizadorContract.AnalizadorEntry._ID, AnalizadorContract.AnalizadorEntry.COLUMN_PREVIOUS_BATERIA_ID,
+                AnalizadorContract.AnalizadorEntry.COLUMN_CURRENT_BATERIA_ID, AnalizadorContract.AnalizadorEntry.COLUMN_BATTERY_STATUS,
+                AnalizadorContract.AnalizadorEntry.COLUMN_CCPM, AnalizadorContract.AnalizadorEntry.COLUMN_MEDIA,
+                AnalizadorContract.AnalizadorEntry.COLUMN_DESV_EST, AnalizadorContract.AnalizadorEntry.COLUMN_PZ,
+                AnalizadorContract.AnalizadorEntry.COLUMN_EXCESSIVE};
+        Cursor cursor = new AnalizadorDBHelper(getApplicationContext()).getReadableDatabase().query(AnalizadorContract.AnalizadorEntry.TABLE_NAME, columns, null, null, null, null, null);
+        String analizadorDB = "";
+        while (cursor.moveToNext()) {
+            analizadorDB += ";" + cursor.getInt(cursor.getColumnIndexOrThrow(columns[0])) + "," + cursor.getInt(cursor.getColumnIndexOrThrow(columns[1])) +
+                    "," + cursor.getInt(cursor.getColumnIndexOrThrow(columns[2])) + "," + cursor.getInt(cursor.getColumnIndexOrThrow(columns[3])) + "," +
+                    cursor.getFloat(cursor.getColumnIndexOrThrow(columns[4])) + "," + cursor.getFloat(cursor.getColumnIndexOrThrow(columns[5])) + "," +
+                    cursor.getFloat(cursor.getColumnIndexOrThrow(columns[6])) + "," + cursor.getFloat(cursor.getColumnIndexOrThrow(columns[7])) + "," +
+                    cursor.getInt(cursor.getColumnIndexOrThrow(columns[8]));
+        }
+        cursor.close();
+        if (!analizadorDB.isEmpty()) {
+            analizadorDB = analizadorDB.substring(1);
+        }
+        try {
+            HttpURLConnection urlConnection = (HttpURLConnection) new URL("https://trabajo-terminal-servidor.uc.r.appspot.com").openConnection();
+            byte[] body = ("{\"data\": \"" + new Bateria(getApplicationContext()).getAllRows() + ":" + analizadorDB + ":" +
+                    new Escaneo(getApplicationContext()).getAllScans() + ":" + new Huella(getApplicationContext()).getAllRows() + "\"}").getBytes();
+            try {
+                urlConnection.setDoOutput(true);
+                urlConnection.setFixedLengthStreamingMode(body.length);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Accion", "Upload");
+                urlConnection.setRequestProperty("Token", getInputData().getString("TOKEN"));
+                OutputStream os = new BufferedOutputStream(urlConnection.getOutputStream());
+                os.write(body);
+                os.flush();
+                os.close();
+                InputStream is = new BufferedInputStream(urlConnection.getInputStream());
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int nRead;
+                while ((nRead = is.read(buffer)) != -1) {
+                    baos.write(buffer, 0, nRead);
+                }
+                Log.i("UploadWorker", baos.toString());
+                return Result.success();
+            } catch (Exception e) {
+                throw new Exception(e);
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (Exception e) {
+            Log.e("HttpURLConnection:", e.toString());
+            return Result.failure();
+        }
+    }
+
+}
