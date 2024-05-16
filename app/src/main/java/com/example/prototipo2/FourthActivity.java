5e963ca9f1c597879cbc2a92b7323f52898cd17a
J-2001 <roldangomezjuan01@gmail.com>
2024-05-16 15:46:04 -0600

    Correción de Errores...

--- app/src/main/java/com/example/prototipo2/FourthActivity.java
+++ app/src/main/java/com/example/prototipo2/FourthActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
